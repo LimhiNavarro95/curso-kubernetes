8da2419f0d2771088cdb5c2cc0f79723275c3ff1
Limhi Navarro <lp.nav95@gmail.com>
2023-07-27 18:49:03 -0600

    Se agrega configuracion de spring cloud kubernetes

--- msvc-cursos/src/main/java/org/nav/springcloud/msvc/cursos/MsvcCursosApplication.java
+++ msvc-cursos/src/main/java/org/nav/springcloud/msvc/cursos/MsvcCursosApplication.java
@@ -2,8 +2,10 @@ package org.nav.springcloud.msvc.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients // -> Se habilita el contexto de feign (web client)
 @SpringBootApplication
 public class MsvcCursosApplication {
--- msvc-usuarios/src/main/java/org/nav/springcloud/msvc/usuarios/msvcusuarios/MsvcUsuariosApplication.java
+++ msvc-usuarios/src/main/java/org/nav/springcloud/msvc/usuarios/msvcusuarios/MsvcUsuariosApplication.java
@@ -2,8 +2,10 @@ package org.nav.springcloud.msvc.usuarios.msvcusuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class MsvcUsuariosApplication {
