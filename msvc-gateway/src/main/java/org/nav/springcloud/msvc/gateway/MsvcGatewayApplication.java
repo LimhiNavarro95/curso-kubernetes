25fa40a97e9395a18ff94102accbf8f4afe85fed
Limhi Navarro <lp.nav95@gmail.com>
2023-08-02 13:46:51 -0600

    Se agrega modulo de micro servicio de gateway que servira como punto de entrada, enrutando los dos servicios que contiene el proyecto

---
+++ msvc-gateway/src/main/java/org/nav/springcloud/msvc/gateway/MsvcGatewayApplication.java
@@ -0,0 +1,15 @@
+package org.nav.springcloud.msvc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsvcGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcGatewayApplication.class, args);
+	}
+
+}
